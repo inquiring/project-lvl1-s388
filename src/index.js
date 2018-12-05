a639433e96e6d349be350f89e194256f62ca8a09
Alexander Suleymanov <evosultanstvo@gmail.com>
2018-12-05 18:29:16 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
-    console.log('Welcome to the Brain Games!');
-    // Wait for user's response.
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!\n`);
-}
+const askForANameAndSayHello = () => {
+  console.log('Welcome to the Brain Games!');
+  // Wait for user's response.
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+};
+
+export default askForANameAndSayHello;
