bc11b541694001e5b2e0fc69f1e2586457afada7
Alexander Suleymanov <evosultanstvo@gmail.com>
2018-12-05 13:53:30 +0300

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+    console.log('Welcome to the Brain Games!');
+    // Wait for user's response.
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!\n`);
+}
