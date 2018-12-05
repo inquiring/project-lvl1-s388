7b8797d210643630e39f30719619e03643acabd3
Alexander Suleymanov <evosultanstvo@gmail.com>
2018-12-06 00:02:49 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 const askForANameAndSayHello = () => {
-  console.log('Welcome to the Brain Games!');
   // Wait for user's response.
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
