c7a222934a96a16298fef2ba85de95ccbf9fea26
Alexander Suleymanov <evosultanstvo@gmail.com>
2018-12-08 11:44:13 +0300

    modified:   src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -11,16 +11,12 @@ const addNumberToSequance = (sequance, maxSequanceLength, step) => {
   return addNumberToSequance([...sequance, nextElem], maxSequanceLength, step);
 };
 
-const getRandomProgression = () => {
-  const progressionLength = 10;
-  const firstElement = getRandomNum(1, 10);
-  const step = getRandomNum(1, 10);
-
-  return addNumberToSequance([firstElement], progressionLength, step);
-};
+const progressionLength = 10;
 
 const getGameData = () => {
-  const sequance = getRandomProgression();
+  const firstElement = getRandomNum(1, 10);
+  const step = getRandomNum(1, 10);
+  const sequance = addNumberToSequance([firstElement], progressionLength, step);
   const hiddenElementPosition = getRandomNum(0, sequance.length - 1);
   const question = sequance.map((elem, index) => (index === hiddenElementPosition ? '..' : elem)).join(' ');
   const answer = `${sequance[hiddenElementPosition]}`;
