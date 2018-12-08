8423b161393eaf91066cbba82ef8e3a7436a874d
Alexander Suleymanov <evosultanstvo@gmail.com>
2018-12-08 15:48:14 +0300

    modified: progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,23 +2,24 @@ import { cons } from 'hexlet-pairs';
 import startGame from '../startGame';
 import getRandomNum from '../utils';
 
-const getGameData = () => {
-  const addNumberToSequance = (sequance, maxSequanceLength, step) => {
-    if (sequance.length === maxSequanceLength) {
-      return sequance;
-    }
-    const lastElem = sequance[sequance.length - 1];
-    const nextElem = lastElem + step;
-    return addNumberToSequance([...sequance, nextElem], maxSequanceLength, step);
-  };
+const getSequance = (sequance, maxSequanceLength, step) => {
+  if (sequance.length === maxSequanceLength) {
+    return sequance;
+  }
+  const lastElem = sequance[sequance.length - 1];
+  const nextElem = lastElem + step;
+  return getSequance([...sequance, nextElem], maxSequanceLength, step);
+};
 
-  const progressionLength = 10;
-  const firstElement = getRandomNum(1, 10);
-  const step = getRandomNum(1, 10);
-  const sequance = addNumberToSequance([firstElement], progressionLength, step);
-  const hiddenElementPosition = getRandomNum(0, sequance.length - 1);
-  const question = sequance.map((elem, index) => (index === hiddenElementPosition ? '..' : elem)).join(' ');
-  const answer = `${sequance[hiddenElementPosition]}`;
+const progressionLength = 10;
+
+const getGameData = () => {
+  const firstElementProgression = [getRandomNum(1, 10)];
+  const stepProgression = getRandomNum(1, 10);
+  const progression = getSequance(firstElementProgression, progressionLength, stepProgression);
+  const hiddenElementPosition = getRandomNum(0, progression.length - 1);
+  const question = progression.map((elem, index) => (index === hiddenElementPosition ? '..' : elem)).join(' ');
+  const answer = `${progression[hiddenElementPosition]}`;
 
   return cons(question, answer);
 };
